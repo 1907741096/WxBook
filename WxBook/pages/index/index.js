c03e6707ab8b021f3e6cf795dc676ba26150fbeb
wzy <wzy@ahstu>
2017-05-08 20:21:16 +0800

    5.8

--- WxBook/pages/index/index.js
+++ WxBook/pages/index/index.js
@@ -1,54 +1,56 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-    // wx.scanCode({
-    //   success: function(res){
-    //     console.log(res.result);
-    //     wx.request({
-    //       url: 'https://api.douban.com/v2/book/isbn/:'+res.result,
-    //       data: {},
-    //       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-    //       header: {"Content-Type": "application/json"}, // 设置请求的 header
-    //       success: function(book){
-    //         // success
-    //         console.log(book);
-    //       },
-    //       fail: function(res) {
-    //         // fail
-    //       },
-    //       complete: function(res) {
-    //         // complete
-    //       }
-    //     })
-    //   },
-    //   fail: function(res) {
-    //     console.log(res);
-    //   },
-    //   complete: function(res) {
-    //     console.log(res);
-    //     // complete
-    //   }
-    // })
-  }
+    data: {
+
+    },
+    onLoad: function (options) {
+        //生命周期函数--监听页面加载
+        wx.request({
+            url: 'https://api.douban.com/v2/book/isbn/:' + res.result,
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            header: { "Content-Type": "application/json" }, // 设置请求的 header
+            success: function (book) {
+                // success
+                console.log(book);
+            },
+            fail: function (res) {
+                // fail
+            },
+            complete: function (res) {
+                // complete
+            }
+        })
+    },
+    onReady: function () {
+        // 生命周期函数--监听页面初次渲染完成
+
+    },
+    onShow: function () {
+        // 生命周期函数--监听页面显示
+
+    },
+    onHide: function () {
+        // 生命周期函数--监听页面隐藏
+
+    },
+    onUnload: function () {
+        // 生命周期函数--监听页面卸载
+
+    },
+    onPullDownRefresh: function () {
+        // 页面相关事件处理函数--监听用户下拉动作
+
+    },
+    onReachBottom: function () {
+        // 页面上拉触底事件的处理函数
+
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角分享
+        return {
+            title: 'title', // 分享标题
+            desc: 'desc', // 分享描述
+            path: 'path' // 分享路径
+        }
+    }
 })
